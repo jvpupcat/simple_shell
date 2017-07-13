abea790b114547d1b5da40701b1e3d097e6238e7
Tope <tope628@gmail.com>
2017-07-13 19:44:09 +0000

    files needed for main shell

---
+++ header.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+typedef struct environment_variable
+{
+	char *name;
+	char *value;
+	struct environment_variable *next;
+} env_v;
+
+extern char **environ;
+void prompt(void);
+char *_getenv(const char *name);
+/* HELPER FUNCTIONS */
+int _strlen(char *str);
+
+#endif
---
+++ shell.c
@@ -0,0 +1,41 @@
+#include "header.h"
+
+/**
+  * main - simple shell
+  * Return: completed command
+  */
+
+int main(int ac, char *av[])
+{
+	char *ptr, *tokens, *store[1024];
+	size_t num;
+	int i, status, success;
+	pid_t pid;
+
+	prompt();
+	while ((success = getline(&ptr, &num, stdin) != -1))
+	{
+		tokens = strtok(ptr," \n\t\r");
+		for(i = 0; tokens != NULL; i++)
+		{
+			store[i] = tokens;
+			tokens = strtok(NULL, " \n\t\r");
+		}
+		store[i] = NULL;
+		pid = fork();
+		if (pid == -1)
+			perror("fork");
+		if (pid == 0)
+		{
+			success = execve(store[0], store, NULL);
+			if (success == -1)
+				printf("Error\n");
+		}
+		else
+		{
+			wait(&status);
+		}
+	prompt();
+	}
+	return (0);
+}
