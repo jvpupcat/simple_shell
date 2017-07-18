617031f727d96dba1c8bc478b1f20567d0aaa898
Tope <tope628@gmail.com>
2017-07-18 22:56:24 +0000

    header

--- header.h
+++ header.h
@@ -7,17 +7,36 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-typedef struct environment_variable
-{
-	char *name;
-	char *value;
-	struct environment_variable *next;
-} env_v;
+#define DELIM "\n\t\r"
 
 extern char **environ;
 void prompt(void);
-char *_getenv(const char *name);
+
 /* HELPER FUNCTIONS */
 int _strlen(char *str);
+char *_strcat(char *dest, char *src);
+void _puts(char *str);
+int _putchar(char c);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+
+/* MAIN FUNCTIONS */
+char *_getenv(const char *name);
+
+/* BUILTIN FUNCTIONS */
+/**
+ * struct builtin_s - struct
+ * @id: variable that finds a match
+ * @function: function that matching variables call
+ * Return: none
+ **/
+typedef struct builtin_s
+{
+	char *id;
+	int (*function)();
+} builtin_t;
+void builtin_id(char *command);
+int sh_exit(void);
+int env(void);
 
 #endif
