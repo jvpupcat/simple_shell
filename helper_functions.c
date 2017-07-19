10a63de765ba37bbcd321aa411eabf65a88e13ea
Tope <tope628@gmail.com>
2017-07-19 00:38:19 +0000

    betty helper

--- helper_functions.c
+++ helper_functions.c
@@ -44,12 +44,12 @@ void _puts(char *str)
 {
 	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i++;
-        }
-        _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
 
 /**
